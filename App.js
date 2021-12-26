58dbf09a2fdef204466225b3ecc794991dcb1b24
Kwabena Agyeman <kagyeman@ymail.com>
2021-12-26 15:09:38 +0000

    set up app logo and splash screen

--- App.js
+++ App.js
@@ -2,14 +2,10 @@
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import AppLoading from "expo-app-loading";
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
 import { LogBox } from "react-native";
 
-import { Marcellus_400Regular } from "@expo-google-fonts/marcellus";
-import { Muli_400Regular } from "@expo-google-fonts/muli";
-import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 
 import MainNavigation from "./src/navigation/MainNavigation";
@@ -20,15 +16,6 @@ LogBox.ignoreLogs([
 ]);
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Muli_400Regular,
-    Marcellus_400Regular,
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
     <>
       <Provider store={store}>
--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,38 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { LogBox } from "react-native";
+import AppLoading from "expo-app-loading";
+
 import { onAuthStateChanged } from "firebase/auth";
 
 import { auth } from "../firebase";
 import { useDispatch, useSelector } from "react-redux";
-import { setAuthentication, setUser } from "../redux/slices/shopSlice";
+import {
+  setAuthentication,
+  setUser,
+  setCheckout,
+} from "../redux/slices/shopSlice";
 
 import AppNavigation from "./AppNavigation";
 import AuthNavigation from "./AuthNavigation";
 
+import { Marcellus_400Regular } from "@expo-google-fonts/marcellus";
+import { Muli_400Regular } from "@expo-google-fonts/muli";
+import { useFonts } from "expo-font";
+
 const MainNavigation = () => {
+  const [appReady, setAppReady] = useState(false);
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.shop.isAuthenticated);
+  let [fontsLoaded] = useFonts({
+    Muli_400Regular,
+    Marcellus_400Regular,
+  });
+
   LogBox.ignoreLogs([
     "Warning: Async Storage has been extracted from react-native core",
   ]);
-  const dispatch = useDispatch();
-  const isAuthenticated = useSelector((state) => state.shop.isAuthenticated);
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
       dispatch(setAuthentication(true));
-      // dispatch(setUser({ uid: user.uid, email: user.email }));
+      dispatch(setUser({ uid: user.uid, email: user.email }));
+
       console.log("User signed in");
-      // console.log(user);
-      // const uid = user.uid;
-      // console.log(
-      //   "User is signed in, see docs for a list of available properties https://firebase.google.com/docs/reference/js/firebase.User",
-      //   uid
-      // );
+      setAppReady(true);
     } else {
       dispatch(setAuthentication(false));
-      // dispatch(setUser({}));
+      dispatch(setUser({}));
+      // dispatch(
+      //   setCheckout({
+      //     id: "",
+      //     webUrl: "",
+      //   })
+      // );
       console.log("User is signed out");
+      setAppReady(true);
     }
   });
+
+  if (!fontsLoaded || !appReady) {
+    return <AppLoading />;
+  }
 
   if (!isAuthenticated) {
     return <AuthNavigation />;
