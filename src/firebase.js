7d1f485c4799eaee581959e9f7dc5629826e7abc
Kwabena Agyeman <kagyeman@ymail.com>
2021-12-27 09:46:42 +0000

    finished cleaning up the app functionalities

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDiAMfux2P830xh04oHE1Hza_wywOKxA78",
-  authDomain: "potent-plasma-333913.firebaseapp.com",
-  projectId: "potent-plasma-333913",
-  storageBucket: "potent-plasma-333913.appspot.com",
-  messagingSenderId: "289849022705",
-  appId: "1:289849022705:web:c05e38ab6501d042ea3150",
-};
+import { firebaseConfig } from "./firebaseCongif";
 
 // !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
---
+++ src/firebaseCongif.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDiAMfux2P830xh04oHE1Hza_wywOKxA78",
+  authDomain: "potent-plasma-333913.firebaseapp.com",
+  projectId: "potent-plasma-333913",
+  storageBucket: "potent-plasma-333913.appspot.com",
+  messagingSenderId: "289849022705",
+  appId: "1:289849022705:web:c05e38ab6501d042ea3150",
+};
