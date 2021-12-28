4f0cbf48b5c65559c5d48e5a76c3074b3cfa1b50
Kwabena Agyeman <kagyeman@ymail.com>
2021-12-28 17:19:28 +0000

    .

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 
-import { firebaseConfig } from "./firebaseCongif";
-
 // !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+// Your web app's Firebase configuration, Fing configuration in Google Firebase Console
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiAMfux2P830xh04oHE1Hza_wywOKxA78",
+  authDomain: "potent-plasma-333913.firebaseapp.com",
+  projectId: "potent-plasma-333913",
+  storageBucket: "potent-plasma-333913.appspot.com",
+  messagingSenderId: "289849022705",
+  appId: "1:289849022705:web:c05e38ab6501d042ea3150",
+};
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
